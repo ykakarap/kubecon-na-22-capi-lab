904a565761fb28a51243bfc72b9198a1b1619cee
Stefan Bueringer <sbueringer@gmail.com>
2022-10-17 15:33:35 +0200

    Bump to CAPI v1.2.4

--- scripts/create-kind-cluster.ps1
+++ scripts/create-kind-cluster.ps1
@@ -4,8 +4,7 @@ kind create cluster --config ./yamls/bootstrap/kind.yaml -n kubecon-na-22-capi-l
 echo "Load pre-downloaded images into kind cluster"
 
 # Load CAPI
-# FIXME(sbueringer)
-kind -n kubecon-na-22-capi-lab load docker-image gcr.io/k8s-staging-cluster-api/capd-manager:v1.2.3 docker.io/sbueringer/cluster-api-controller:v1.2.4-preview k8s.gcr.io/cluster-api/kubeadm-bootstrap-controller:v1.2.3 k8s.gcr.io/cluster-api/kubeadm-control-plane-controller:v1.2.3
+kind -n kubecon-na-22-capi-lab load docker-image gcr.io/k8s-staging-cluster-api/capd-manager:v1.2.4 k8s.gcr.io/cluster-api/cluster-api-controller:v1.2.4 k8s.gcr.io/cluster-api/kubeadm-bootstrap-controller:v1.2.4 k8s.gcr.io/cluster-api/kubeadm-control-plane-controller:v1.2.4
 
 # Load cert-manager
 kind -n kubecon-na-22-capi-lab load docker-image quay.io/jetstack/cert-manager-cainjector:v1.9.1 quay.io/jetstack/cert-manager-controller:v1.9.1 quay.io/jetstack/cert-manager-webhook:v1.9.1
